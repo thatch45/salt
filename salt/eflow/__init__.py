5483ef7fd70b4cd371ba9ab692293cdc26282efe
Thomas S Hatch <thatch45@gmail.com>
2015-06-11 22:17:16 -0600

    Start on the eflow module system

---
+++ salt/eflow/__init__.py
@@ -0,0 +1,8 @@
+'''
+The eflow system allows for advanced event tracking and reactions
+'''
+# Needed:
+# Use a top file to load sls files locally
+# use the existing state system to compile a low state
+# Create a new state runtime to run the low state flow programming style
+# Create the eflow plugin system
