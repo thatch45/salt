e23692542b0a257a0c819584618e3db3c79abf2a
Thomas S Hatch <thatch45@gmail.com>
2015-06-12 00:03:13 -0600

    initial add of reg eflow module

---
+++ salt/eflow/reg.py
@@ -0,0 +1,26 @@
+'''
+The functions that manage the eflow register
+'''
+# Import python libs
+import fnmatch
+
+
+# TODO: This is just a stub function to test the system, this will need
+# to be replaced!
+def set_(name, add, match):
+    '''
+    Add a value to the named set when the matched tag is found
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'comment': '',
+           'result': True}
+    reg_key = '{0}_|-set'.format(name)
+    if reg_key not in __reg__:
+        __reg__[reg_key] = set()
+    for event in __events__:
+        if fnmatch.fnmatch(event['tag'], name):
+            val = event['data'].get()
+            ret['changes'][add] = val
+            __reg__[reg_key].add(val)
+    return ret
