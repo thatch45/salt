d3d77995503a4e7584fc988fb1a993836c4da03b
Thomas S Hatch <thatch45@gmail.com>
2015-06-12 22:03:23 -0600

    Add a simple eflow state to test the register

---
+++ salt/eflow/test.py
@@ -0,0 +1,23 @@
+'''
+A simple test module, write things to disk to verify activity
+'''
+# Import python libs
+import os
+import json
+
+# Import salt libs
+import salt.utils
+
+
+def save(name):
+    '''
+    Save the named register to /tmp/<name>
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'comment': '',
+           'result': True}
+    fn_ = os.path.join('/tmp', name)
+    with salt.utils.fopen(fn_, 'w+') as fp_:
+        fp_.write(json.dumps(__reg__))
+    return ret
